7db25b2c9903f89595c44a446acaf94f5cf31a89
Akshay <Akshay.Rathore06@gmail.com>
2020-04-13 12:35:33 +0530

    added code for two player game

--- src/Game/StartGame.java
+++ src/Game/StartGame.java
@@ -8,6 +8,7 @@ public class StartGame {
 	int guessLimit;
 	int targetDigit;
 	int count=0;
+	boolean won=false;
 	public void host()
 	{
 		
@@ -18,28 +19,50 @@ public class StartGame {
 		System.out.println("I`m guessing a digit between 0 and "+guessLimit+"...");
 		
 		targetDigit=(int) (Math.random()*guessLimit);
+		System.out.println(targetDigit); //TODO remove this line
 		Player player = new Player();
+		Player player2=new Player();
 		int playerGuess = 0;
+		int playerGuess2 = 0;
 		
 		
-		while(true) {
+		/*
+		 * while(true) { playerGuess = player.play(); playerGuess2 = player2.play();
+		 * if(playerGuess==targetDigit) {
+		 * System.out.println("Congratulations,you win! player 1");
+		 * 
+		 * break; } else if(playerGuess2==targetDigit) {
+		 * System.out.println("Congratulations,you win! player 2"); break; } else {
+		 * System.out.println("Sorry that guess was incorrect, try again!");
+		 * 
+		 * count++; if (count==3) { System.out.println("Sorry, you`re out of guesses");
+		 * break; } } }
+		 */
+		
+		for(int i=0;i<3;i++) {
 			playerGuess = player.play();
+			playerGuess2 = player2.play();
 			if(playerGuess==targetDigit) {
-				System.out.println("Congratulations, you win!");
+				System.out.println("Congratulations,you win! player 1");
+				won=true;
+				break;
+			}
+			else if(playerGuess2==targetDigit)
+			{
+				System.out.println("Congratulations,you win! player 2");
+				won=true;
 				break;
 			}
 			else {
 				System.out.println("Sorry that guess was incorrect, try again!");
-				
-				count=count+1;
-				if (count==3)
-				{
-					System.out.println("Sorry, you`re out of guesses");
-		            break;
-				}
 			}
 		}
 		
+		if(!won) {
+			System.out.println("You could not guess correctly");
+		}
+		
+		System.out.println("Game Over!");
 		
 	}
 
